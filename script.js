d193a481ca851f0b95c08e69646aa4934170e972
mrh2020ez-jpg <mr.h.2020.ez@gmail.com>
2025-12-28 18:26:00 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-function updateDashboard() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString('fa-IR');
-    const dateString = new Intl.DateTimeFormat('fa-IR', { dateStyle: 'full' }).format(now);
-    
-    document.getElementById('live-clock').innerHTML = `<span>${dateString}</span> | <strong>${timeString}</strong>`;
+function showMessage() {
+    alert("این وب سایت به عنوان پروژه دانشگاهی طراحی شده است.");
 }
-
-// اسکرول نرم بین بخش‌ها
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            e.preventDefault();
-            window.scrollTo({ top: targetElement.offsetTop - 50, behavior: 'smooth' });
-        }
-    });
-});
-
-setInterval(updateDashboard, 1000);
-updateDashboard();
