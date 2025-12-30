e56d0760265198f0a513cc71f525c83e57cf1516
mrh2020ez-jpg <mr.h.2020.ez@gmail.com>
2025-12-30 16:13:49 +0330

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        alert("محصول به سبد خرید اضافه شد (دموی دانشگاهی)");
-    });
-});
